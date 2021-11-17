201f1b34e21a8d2a631abf57d6f10da7caa4fd91
EndofSmallSanctuary <kravchenkov@p310.rniirs.ru>
2021-11-17 16:44:04 +0300

    fusion implemented

--- app/src/main/java/com/example/smarttag/Services/GpsService.java
+++ app/src/main/java/com/example/smarttag/Services/GpsService.java
@@ -17,6 +17,7 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.PowerManager;
 import android.util.Log;
 
@@ -28,6 +29,10 @@ import androidx.core.content.ContextCompat;
 
 import com.example.smarttag.PresentationActivity;
 import com.example.smarttag.R;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.Date;
 import java.util.List;
@@ -44,29 +49,23 @@ public class GpsService extends Service {
     Boolean isBinded = false;
     Boolean isAlive = false;
     PowerManager.WakeLock wakeLock;
+
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            Log.d("status","location changed");
+            Location offeredLocation = locationResult.getLastLocation();
+
+            Log.d("status","current actual location will be at "+offeredLocation.getTime());
+            actualLocation = offeredLocation;
+        }
+    };
+
     private final LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(@NonNull Location location) {
-            Log.d("status","location changed");
-            List<String> locationProviders = locationManager.getProviders(true);
-            Location offeredLocation = null;
-            for (String provider : locationProviders){
-                @SuppressLint("MissingPermission") Location l  = locationManager.getLastKnownLocation(provider);
-                if(l==null){
-                    continue;
-                }
-                if(validateLocation(l)) {
-                    if (offeredLocation == null || l.getAccuracy() < offeredLocation.getAccuracy()) {
-                        offeredLocation = l;
-                    }
-                }
-                Log.d("status",provider+" offers a location at "+l.getTime());
-            }
 
-            if (offeredLocation!=null) {
-                Log.d("status","current actual location will be at "+offeredLocation.getTime());
-                actualLocation = offeredLocation;
-            };
         }
 
         @Override
@@ -139,22 +138,19 @@ public class GpsService extends Service {
         return START_NOT_STICKY;
     }
 
+    @SuppressLint("MissingPermission")
     private void initiateProcessing() {
         try {
-            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-            if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                Toasty.error(getApplicationContext(),"Error during start Gps Service",Toasty.LENGTH_SHORT).show();
-                return;
-            }
-            if(locationManager!=null){
-                long UPDATE_INTERVAL = 1000;
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                                UPDATE_INTERVAL,0,locationListener);
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                        UPDATE_INTERVAL,0,locationListener);
-               // actualLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            }
-        } catch (Exception e){
+
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setInterval(2000);
+            locationRequest.setFastestInterval(1000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+            LocationServices.getFusedLocationProviderClient(this)
+                    .requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper());
+
+          } catch (Exception e){
             Log.d("exp",e.getMessage());
         }
     }
@@ -175,6 +171,8 @@ public class GpsService extends Service {
 
     @Override
     public void onDestroy() {
+        LocationServices.getFusedLocationProviderClient(this)
+                .removeLocationUpdates(locationCallback);
         stopForeground(true);
         stopSelf();
         isAlive = false;
